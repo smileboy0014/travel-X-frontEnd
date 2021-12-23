9b00d664fe9d03cbe0d5e19857ac0c8fe5a42ecc
skbiotekmes <skbt@com>
2021-12-23 11:48:47 +0900

    비율 수정

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.main}>
       <Link href="/search" as={`/search`}>
         <a>
-          <img src="/main.png" />
+          <img src="/main.jpg" />
         </a>
       </Link>
     </div>
