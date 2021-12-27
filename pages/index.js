eb57b2b1a172adda245c1ff8d44f46ff9ef1b820
ezro84 <65005333+ezro84@users.noreply.github.com>
2021-12-27 17:21:36 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from "react";
 import Link from "next/link";
-import Script from 'next/script';
+import Script from "next/script";
 import styles from "../styles/Home.module.css";
-import GeoLocationButton from '../components/Button/GeolocationButton';
-import SearchMapModal from './../components/Modal/Map/SearchMapModal';
+import GeoLocationButton from "../components/Button/GeolocationButton";
+import SearchMapModal from "./../components/Modal/Map/SearchMapModal";
 import { useDispatch, useSelector } from "react-redux";
 import * as mapActions from "../redux/store/modules/map";
-import SelectSearchLocationModal from './../components/Modal/Map/SelectSearchLocationModal';
+import SelectSearchLocationModal from "./../components/Modal/Map/SelectSearchLocationModal";
 import MainTap from "../components/Tap/MainTap";
 
 export default function Home() {
   const [showSearchMapModal, setShowSearchMapModal] = useState(false);
-  const [showSelectSearchLocationModal, setShowSelectSearchLocationModal] = useState(false);
+  const [showSelectSearchLocationModal, setShowSelectSearchLocationModal] =
+    useState(false);
   const dispatch = useDispatch();
   const { addresses } = useSelector((state) => state.map);
 
   const handleScriptLoaded = (value) => {
     dispatch(mapActions.setScriptLoaded(value));
-  }
+  };
 
   return (
     <div className={styles.main}>
@@ -25,16 +26,22 @@ export default function Home() {
         type="text/javascript"
         src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ra1rvd631l&submodules=geocoder"
         onError={(e) => {
-          console.error('Script failed to load', e);
+          console.error("Script failed to load", e);
         }}
-        onLoad={() => { handleScriptLoaded(true) }}
+        onLoad={() => {
+          handleScriptLoaded(true);
+        }}
       />
       <Link href="/search" as={`/search`}>
         <a>
           <img src="/main.jpg" />
         </a>
       </Link>
-      <GeoLocationButton showSelectSearchLocationModal={() => setShowSelectSearchLocationModal(true)} />
+      <GeoLocationButton
+        showSelectSearchLocationModal={() =>
+          setShowSelectSearchLocationModal(true)
+        }
+      />
       <SelectSearchLocationModal
         onClose={() => setShowSelectSearchLocationModal(false)}
         show={showSelectSearchLocationModal}
@@ -44,14 +51,14 @@ export default function Home() {
         onClose={() => setShowSearchMapModal(false)}
         show={showSearchMapModal}
       />
-      <p>{addresses.map((addr, index) => (
+      <p>
+        {addresses.map((addr, index) => (
           <Fragment key={index}>
             {addr}
             <br />
           </Fragment>
         ))}
       </p>
-       
     </div>
   );
 }
