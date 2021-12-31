b5678d8d79cf71a68947aefd22fdc44e9e7615e2
skbiotekmes <skbt@com>
2021-12-31 16:29:05 +0900

    디자인 수정중

--- components/Card/SearchResultList.js
+++ components/Card/SearchResultList.js
@@ -1,7 +1,8 @@
 import { useRef, forwardRef, useState, useEffect } from "react";
 import SearchResultCarousel from "./Carousel/SearchResultCarousel";
 import style from "../../styles/Carousel.module.css";
-import SearchResultCard from "./SearchResultCard";
+// import SearchResultCard from "./SearchResultCard";
+import SearchResultCard from "./SearchCard";
 import Link from "next/link";
 
 const SearchResultList = (props, ref) => {
@@ -23,47 +24,45 @@ const SearchResultList = (props, ref) => {
   }, []);
 
   return (
-    <div className={style.container}>
+    <div>
       <div>
-        <div>
-          {rooms.item &&
-            rooms.item.map((room, index) => {
-              if (rooms.item.length === index + 1) {
-                return <div ref={ref} key={room}></div>;
-              } else {
-                return (
-                  <div key={index}>
-                    <div style={{ border: "1px solid rgb(0, 0, 0)" }}>
-                      <SearchResultCarousel
+        {rooms.item &&
+          rooms.item.map((room, index) => {
+            if (rooms.item.length === index + 1) {
+              return <div ref={ref} key={room}></div>;
+            } else {
+              return (
+                <div key={index}>
+                  <div>
+                    {/* <div style={{ border: "1px solid rgb(0, 0, 0)" }}> */}
+                    {/* <SearchResultCarousel
                         ref={carouselRef}
                         items={room.images}
                         initSlide={slide}
-                      />
-                      <Link
-                        href="/view/detail/[id]"
-                        as={`/view/detail/${room.roomId}`}
-                        key={index}
-                      >
-                        <a>
-                          <SearchResultCard
-                            id={room.roomId}
-                            roomName={room.roomName}
-                            propertyName={room.propertyName}
-                            images={room.images}
-                            maxUser={room.maxUser}
-                            price={room.price}
-                            ratingScoreAvg={GetRandomRatingScore()}
-                          />
-                        </a>
-                      </Link>
-                    </div>
+                      /> */}
+                    <Link
+                      href="/view/detail/[id]"
+                      as={`/view/detail/${room.roomId}`}
+                      key={index}
+                    >
+                      <a>
+                        <SearchResultCard
+                          id={room.roomId}
+                          roomName={room.roomName}
+                          propertyName={room.propertyName}
+                          images={room.images}
+                          maxUser={room.maxUser}
+                          price={room.price}
+                          ratingScoreAvg={GetRandomRatingScore()}
+                        />
+                      </a>
+                    </Link>
                   </div>
-                );
-              }
-            })}
-        </div>
+                </div>
+              );
+            }
+          })}
       </div>
-      <p>____</p>
     </div>
   );
 };
