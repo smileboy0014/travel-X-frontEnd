f75f29b77eddfc587d0a32ca18fe472c3710d0ae
Choi si young <msc200353@naver.com>
2022-02-13 17:53:03 +0900

    SRP 구글맵 컴포넌트 수정

--- components/Modal/Map/ListDetailGoogleMap.js
+++ components/Modal/Map/ListDetailGoogleMap.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, Fragment } from "react";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect, useState, Fragment, useCallback } from "react";
+import { GoogleMap, useLoadScript, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { isMobile } from "react-device-detect";
 import { useSelector } from "react-redux";
 import SearchMobileCard from "../../Card/SearchMobileCard";
@@ -10,30 +10,54 @@ const containerStyle = {
 };
 
 var MARKER_ICON_URL =
-// "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
 "https://ssl.pstatic.net/static/maps/img/icons/sp_pins_spot_v3.png";
 var MARKER_HIGHLIGHT_ICON_URL =
   "https://ssl.pstatic.net/static/maps/img/icons/sp_pins_spot_v3_over.png";
 
-var mobileWindows = [];
-
 const DetailMap = ({ lat, lng, onRequestClosed }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBVi31CDrC3Dyi8f166yV_-6WKocsk7I8E"
+  });
   const searchDataValue = useSelector(({ searchResult }) => searchResult.data);
   const [roomData, setRoomData] = useState([]);
   const [markers, setMarkers] = useState([]);
-  const [mapCenter, setMapCenter] = useState({lat: '', lng: ''})
+  const [mobileWindows, setMobileWindows] = useState([]);
+  const [mapCenter, setMapCenter] = useState({lat: 37.4959854, lng: 127.0664091});
 
   const addRoomMapMarker = () => {
     console.log(searchDataValue[0]);
-    if (searchDataValue[0] !== undefined && searchDataValue[0].length > 0) {
+    if (searchDataValue[0] !== undefined && searchDataValue[0].length > 0 && isLoaded) {
       let tmpMarkers = [];
+      let tmpMobileWindows = [];
       searchDataValue[0].map((room) => {
         let marker = {
+          id: room.roomId + room.useType,
           position: {
             lat: room.location.lat,
             lng: room.location.lon
           }
         }
+
+        let mobileContent = [
+          {
+            img: room.images[0],
+            type: room.propertyType,
+            name: room.propertyName,
+            averageScore: room.reviewSummary.averageScore,
+            reviewCount: room.reviewSummary.reviewCount,
+            price: room.basePrice,
+          },
+          {
+            img: room.images[0],
+            type: room.propertyType,
+            name: room.propertyName,
+            averageScore: room.reviewSummary.averageScore,
+            reviewCount: room.reviewSummary.reviewCount,
+            price: room.basePrice,
+          }
+        ];
+        
+        tmpMobileWindows.push(mobileContent);
         tmpMarkers.push(marker);
       });
 
@@ -41,26 +65,35 @@ const DetailMap = ({ lat, lng, onRequestClosed }) => {
         lat: searchDataValue[0][0].location.lat,
         lng: searchDataValue[0][0].location.lon
       });
-      
-      setMarkers(markers.concat(tmpMarkers));
+      setMarkers(tmpMarkers);
+      setMobileWindows(tmpMobileWindows);
     }
+  };
+
+  const handleActiveMarker = (index) => {
+    setRoomData(mobileWindows[index]);
+  };
+
+  const handleOnLoad = (map) => {
+    const bounds = new google.maps.LatLngBounds();
+    markers.forEach(({ position }) => bounds.extend(position));
+    map.fitBounds(bounds);
   };
 
   useEffect(() => {
     setMarkers([]);
     addRoomMapMarker();
-  }, [searchDataValue]);
+  }, [searchDataValue, isLoaded]);
 
   useEffect(() => {
-    console.log(markers);
-  }, [markers]);
+    console.log(roomData);
+  }, [roomData])
 
   return (
     <div>
-      <LoadScript
-        googleMapsApiKey="AIzaSyBVi31CDrC3Dyi8f166yV_-6WKocsk7I8E"
-      >
+      {isLoaded ? (
         <GoogleMap
+          onLoad={handleOnLoad}
           id='list-detail-map'
           mapContainerStyle={containerStyle}
           center={mapCenter}
@@ -69,20 +102,29 @@ const DetailMap = ({ lat, lng, onRequestClosed }) => {
           {markers.map((marker, index) => (
             <Fragment key={index}>
               <Marker
+                id={marker.id}
                 position={marker.position}
-              />
+                onClick={() => handleActiveMarker(index)}
+              >
+                {/* {activeMarker === marker.id ? (
+                  <InfoWindow 
+                    position={marker.position}
+                    onCloseClick={() => setActiveMarker(null)}>
+                    <div>{marker.id}</div>
+                  </InfoWindow>
+                ) : null} */}
+              </Marker>
             </Fragment>
           ))}
-         
-          <></>
           <SearchMobileCard
             data={roomData}
             closeModal={(value) => {
               onRequestClosed(value);
             }}
-          ></SearchMobileCard>
+          />
         </GoogleMap>
-      </LoadScript>
+      ) : null}
+      
     </div>
   );
 };
